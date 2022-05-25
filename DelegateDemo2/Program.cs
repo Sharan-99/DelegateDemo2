55ccc99a1f3b009dfad75cba2cf5e75b933fb1b2
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 10:19:30 +0530

    first commit

---
+++ DelegateDemo2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo2
+{
+    public delegate void Greet(string input);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greet obj = delegate (string input)
+            {
+                Console.WriteLine("Hi " + input + ", Good Morning!");
+            };
+
+            obj("Sharan");
+
+            Console.ReadKey();
+        }
+    }
+}
